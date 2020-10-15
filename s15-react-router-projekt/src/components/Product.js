3ed5f01d3fdc20d1f368fd71282d062bc9072f41
Lukasz Mularczyk <lukmul.work@gmail.com>
2020-10-15 09:50:22 +0200

    add dupa

--- s15-react-router-projekt/src/components/Product.js
+++ s15-react-router-projekt/src/components/Product.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Product = (props) => {
     return (
         <article>
-            <h1>Nazwa produktu:</h1>
+            <h1>Nazwa proddupauktu:</h1>
             <h3>{props.productName}</h3>
             <p>?!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, repellendus explicabo ad at hic ut adipisci ratione magnam culpa fugiat ipsa iusto eligendi temporibus repellat veniam quisquam illum fugit maiores.</p>
             <p>{props.productName} jeździ.</p>
