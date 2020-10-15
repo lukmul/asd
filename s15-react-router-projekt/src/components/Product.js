7e716755881256131855b07b2e84021264d3270d
itosu <janwilczak@gmail.com>
2020-10-15 09:42:57 +0200

    Added ?

--- s15-react-router-projekt/src/components/Product.js
+++ s15-react-router-projekt/src/components/Product.js
@@ -5,7 +5,7 @@ const Product = (props) => {
         <article>
             <h1>Nazwa produktu</h1>
             <h3>{props.productName}</h3>
-            <p>!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, repellendus explicabo ad at hic ut adipisci ratione magnam culpa fugiat ipsa iusto eligendi temporibus repellat veniam quisquam illum fugit maiores.</p>
+            <p>?!Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, repellendus explicabo ad at hic ut adipisci ratione magnam culpa fugiat ipsa iusto eligendi temporibus repellat veniam quisquam illum fugit maiores.</p>
             <p>{props.productName} jeździ.</p>
         </article>
 
