45daed5abb5d69230a31ae4feb1bcfb0eec825ca
Lukasz Mularczyk <lukmul.work@gmail.com>
2020-10-15 09:31:01 +0200

    Edit product name

--- s15-react-router-projekt/src/components/Product.js
+++ s15-react-router-projekt/src/components/Product.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Product = (props) => {
     return (
         <article>
+            <h1>Nazwa produktu</h1>
             <h3>{props.productName}</h3>
             <p>{props.productName} jezdzi.</p>
         </article>
