2004773de0123772e58bfa6c46364ad1b25d95d9
bot-lukmul <72913124+bot-lukmul@users.noreply.github.com>
2020-10-15 10:43:44 +0200

    Update Product.js
    even more ultimate changes

--- s15-react-router-projekt/src/components/Product.js
+++ s15-react-router-projekt/src/components/Product.js
@@ -4,8 +4,8 @@ const Product = (props) => {
     return (
         <article>
             <h1>@Nazwa produktu:</h1>
-            <h3>6{props.productName}</h3>
-            <p>-Lorem dolor sit amet consectetur adipisicing elit. Sint, repellendus explicabo ad at hic ut adipisci ratione magnam culpa fugiat ipsa iusto eligendi temporibus repellat veniam quisquam illum fugit maiores.</p>
+            <h3>7{props.productName}</h3>
+            <p>-Lorem ndi temporibus repellat veniam quisquam illum fugit maiores.</p>
             <p>{props.productName}ipsum jeździ.</p>
         </article>
 
