19337f159e7259b730218efe6540045858e36299
Lukasz Mularczyk <lukmul.work@gmail.com>
2020-10-15 09:56:17 +0200

    My changes

--- s15-react-router-projekt/src/components/Product.js
+++ s15-react-router-projekt/src/components/Product.js
@@ -4,7 +4,7 @@ const Product = (props) => {
     return (
         <article>
             <h1>@Nazwa proddupauktu:</h1>
-            <h3>{props.productName}</h3>
+            <h3>6{props.productName}</h3>
             <p>-Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, repellendus explicabo ad at hic ut adipisci ratione magnam culpa fugiat ipsa iusto eligendi temporibus repellat veniam quisquam illum fugit maiores.</p>
             <p>{props.productName} jeździ.</p>
         </article>
