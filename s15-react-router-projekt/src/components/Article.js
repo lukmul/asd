4ff82fc054a16e7711992b00bed1f6cd851d0399
Lukasz Mularczyk <lukmul.work@gmail.com>
2020-10-16 12:34:11 +0200

    article, Product changes

--- s15-react-router-projekt/src/components/Article.js
+++ s15-react-router-projekt/src/components/Article.js
@@ -8,8 +8,8 @@ const Article = ({ title, author, text }) => {
 
     return (
         <article style={styles}>
-            <h3 style={{ marginBottom: 3, textTransform: "uppercase" }}>{title}</h3>
-            <span style={{ marginBottom: 10, display: "block", fontSize: 12 }}>{author}</span>
+            <h3 style={{ marginBottom 3, textTransform: "uppercase" }}>{title}</h3>
+            <span style={{ marginBottom: 110, display: "block", fontSize: 10 }}>{author}</span>
             <p style={{ color: "blue", fontSize: 14 }}>{text}</p>
             <p>asdasd</p>
         </article>
