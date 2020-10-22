67e7dbf702a554f3d36f4e0050afdeebe5ef822f
Lukasz Mularczyk <lukmul.work@gmail.com>
2020-10-22 09:24:33 +0200

    another dumb commit

--- s15-react-router-projekt/src/components/Product.js
+++ s15-react-router-projekt/src/components/Product.js
@@ -4,7 +4,7 @@ const Product = (props) => {
     return (
         <article>
             <h1>Nazwa produktu:</h1>
-            <h3>00{props.productName}</h3>
+            <h3><span><h1>Bla bla bla</h1></span></h3>
             <h4>Jakas dziwna tresc</h4>
             <p>{props.productName}ipsum jeździ.</p>
             <p>wiecej informacji</p>
